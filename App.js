c8ee1e088c660e23ae158ac2b34a3e03532098f1
doannx <doannx@gmail.com>
2017-10-27 20:31:20 +0700

    Add navigator dependency & Init screens.

--- App.js
+++ App.js
@@ -1,57 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
+class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View>
+        <Text>HomeScreen</Text>
+        <Button
+          title="Go to PostScreen"
+          onPress={() => {
+            this.props.navigation.navigate('post');
+          }}
+        >
+        </Button>
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+class PostScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>PostScreen</Text>
+        <Button
+          title="Go to PostCommentsScreen"
+          onPress={() => {
+            this.props.navigation.navigate('postComments');
+          }}
+        />
+      </View>
+    )
+  }
+}
+
+class PostCommentsScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>PostCommentsScreen</Text>
+      </View>
+    )
+  }
+}
+
+const AppNavigator = new StackNavigator({
+  home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      title: '#ChuyenCuaDev'
+    }
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  post: {
+    screen: PostScreen,
+    navigationOptions: {
+      title: '#ChuyenCuaDev'
+    }
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  postComments: {
+    screen: PostCommentsScreen,
+    navigationOptions: {
+      title: '#ChuyenCuaDev'
+    }
+  }
 });
+
+export default AppNavigator;
